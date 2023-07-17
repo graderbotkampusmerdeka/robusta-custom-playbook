8e5e74b26fd7f64067fe5d74ab57220123667a78
Novian Deny <anggerdeni@gmail.com>
2023-07-17 13:52:37 +0700

    update

--- custom_playbook/action.py
+++ custom_playbook/action.py
@@ -3,10 +3,7 @@ import logging
 
 @action
 def custom_action(alert: PrometheusKubernetesAlert):
-    # we have full access to the pod on which the alert fired
-    labels = alert.alert.labels
-    if 'deployment' in labels:
-        logging.info(f"[custom_action] executing custom_action for {labels['deployment']}")
-    else:
-        logging.info(f"[custom_action] executing custom_action for {labels}")
+    alert_subject = alert.get_alert_subject()
+
+    logging.info(f"[custom_action] executing custom_action for {alert_subject.subject_type.value} - {alert_subject.name}")
 
